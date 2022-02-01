521354763e22fc3bb1ae80915ae1fed14e2285cf
jaskirat <sjaskirat199@gmail.com>
2022-02-02 00:10:15 +0530

    mini app

---
+++ src/Basics.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+function Basics() {
+  let [count, setCount] = useState(0);
+
+  // case 1 when dependency array is empty 
+  // it works like componentDidMount in class component
+  // i.e. its is executed only once after first render
+  useEffect(()=>{
+    console.log("use effect was executed")
+  },[])
+
+  // case 2 
+  // when we omit dependency array , use efect will execute after every render
+  // can not update state in this case , as it runs after every render , will end in infinite loop 
+  // and will give error
+  useEffect(()=>{
+    console.log("use effect case 2 was executed");
+  })
+
+
+  console.log("component was rendered");
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+      <p>{count}</p>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        -
+      </button>
+    </div>
+  );
+}
+
+export default Basics;
